08b73bc2bf767a8293c10f4d8b4c0a6d6d33c75a
Mecasu <melihcansunay@live.com>
2024-06-06 17:17:13 +0300

    task 1 done.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,27 @@
 package org.example;
 
 
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(checkForPalindrome("I did, did I?"));
+        System.out.println(checkForPalindrome("Racecar"));
+        System.out.println(checkForPalindrome("hello"));
+        System.out.println(checkForPalindrome("Was it a car or a cat I saw ?"));
+
     }
+
+    public static boolean checkForPalindrome(String str) {
+        Locale.setDefault(new Locale("en", "GB"));
+        str = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        String reverseStr = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverseStr = reverseStr + str.charAt(i);
+        }
+        return str.equals(reverseStr);
+    }
+
 
 }
